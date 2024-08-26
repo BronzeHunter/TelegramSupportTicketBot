3852644765bf4de6705e1fad4318033ddbc57106
BronzeHunter <121955265+BronzeHunter@users.noreply.github.com>
2024-08-27 02:39:42 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Токен бота
+BOT_TOKEN = 'your bot token'
+
+# ID чата администраторов (групповой чат)
+ADMIN_CHAT_ID = 'your chat id'
+
+# Список администраторов с их именами
+ADMIN_USERS = {
+    'your id': 'Менеджер',
+    'your id': 'Администратор'
+}
